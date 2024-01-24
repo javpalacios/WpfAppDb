412bd534830de3f64b44ac78a8184c7df1cab56c
Javier <javier.palacios@iesluisvives.org>
2024-01-24 20:27:16 +0100

    Add project files.

---
+++ WpfAppDb/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppDb
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfAppDb/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfAppDb.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int? DepartmentId { get; set; }
+    public string? ImageUri { get; set; }
+    public string? Position { get; set; }
+    public virtual Department? Department { get; set; }
+}
